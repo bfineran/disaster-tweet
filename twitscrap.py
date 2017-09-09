bb0aaaf1d8ab0b0b0c793640a3fc6433bbc8053a
hanmingz <hanmingz@seas.upenn.edu>
2017-09-09 17:17:29 -0400

    searchquery function

--- twitscrap.py
+++ twitscrap.py
@@ -5,26 +5,40 @@ import datetime
 import json
 
 import twitterscraper
-print(os.path.dirname(twitterscraper.__file__))
 
+def searchquery(keyword, numsearches, filename):
+	for tweet in twitterscraper.query.query_tweets(keyword, numsearches)[:numsearches]:
+		text = tweet.text.encode('utf-8')
+		try:
+			addr, confidence = usaddress.tag(text)
+		except:
+			continue
+
+		if confidence is not "Ambiguous":
+			print(tweet.user.encode('utf-8'))
+			filename.write(tweet.user.encode('utf-8'))
+			print(tweet.timestamp)
+			timestr = tweet.timestamp.strftime("%Y-%m-%d %H:%M:%S")
+			filename.write(timestr)
+			print(text)
+			filename.write(text)
+			print(addr)
+			filename.write(json.dumps(dict(addr)))
+			filename.write("\n\n\n")
+
+	return
+
+print("HELOOWOWSOISOHSFIU")
 
 F = open("tweetsfile.txt", "w")
+Q = open("searchterms.txt", "r")
 
-for tweet in twitterscraper.query.query_tweets("@houstonoem help", 1000)[:1000]:
-	text = tweet.text.encode('utf-8')
-	try:
-		addr, confidence = usaddress.tag(text)
-	except:
-		continue
 
-	if confidence is not "Ambiguous":
-		print(tweet.user.encode('utf-8'))
-		F.write(tweet.user.encode('utf-8'))
-		print(tweet.timestamp)
-		timestr = tweet.timestamp.strftime("%Y-%m-%d %H:%M:%S")
-		F.write(timestr)
-		print(text)
-		F.write(text)
-		print(addr)
-		F.write(json.dumps(dict(addr)))
-		F.write("\n\n\n")
+
+
+for line in Q:
+	print("New Query:")
+	print("\n\n\n")
+	searchquery(line, 1000, F)
+	print("\n\n\n")
+	print("finished search for " + line)
